4db766476d1aba63a71ba055b40126be49240cda
Neil Bishop <byemreaydin289@outlook.com>
2025-09-12 23:52:19 +0300

    route problem solved

--- routes/puzzles.js
+++ routes/puzzles.js
@@ -48,7 +48,7 @@ puzzlesRouter.get("/puzzleSize", async (req, res) => {
     }
 })
 
-puzzlesRouter.post("reportPuzzle", async (req, res) => {
+puzzlesRouter.post("/reportPuzzle", async (req, res) => {
     try {
         const { puzzleId } = req.body;
 

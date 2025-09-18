522436a58def9f1ed1fab759e3d02aee9a1bf2a0
Neil Bishop <byemreaydin289@outlook.com>
2025-09-18 21:37:42 +0300

    vercel build

--- src/api.js
+++ src/api.js
@@ -1,34 +1,30 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const serverless = require('serverless-http')
-const path = require("path")
-require('dotenv').config()
+const express = require("express");
+const mongoose = require("mongoose");
+const serverless = require("serverless-http");
+const path = require("path");
+require("dotenv").config();
 
-const PORT = process.env.PORT || 3000;
 const app = express();
-
 const DB = process.env.DB;
 
-app.use(express.json())
+app.use(express.json());
 
-const puzzlesRouter = require("../routes/puzzles")
+const puzzlesRouter = require("../routes/puzzles");
 
 mongoose
-    .connect(DB)
-    .then(() => {
-        console.log("Connection Successful");
-    }).catch((e) => {
-        console.log(e);
-    });
+  .connect(DB)
+  .then(() => {
+    console.log("Connection Successful");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 
 app.get("/", (_req, res) => {
-    res.sendFile(path.join(__dirname, '../dist/index.html'))
-})
+  res.sendFile(path.join(__dirname, "../dist/index.html"));
+});
 
-app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server Started at ${PORT}`)
-})
+app.use("/", puzzlesRouter);
 
-app.use('/', puzzlesRouter)
-
-module.exports.handler = serverless(app)
+module.exports = app;
+module.exports.handler = serverless(app);
